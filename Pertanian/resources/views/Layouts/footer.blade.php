24ee2be938199f108885f877c8f21cd6590578ce
RivaelM <118703792+RivaelM@users.noreply.github.com>
2023-05-17 07:03:33 +0700

    Source Code

--- Pertanian/resources/views/Layouts/footer.blade.php
+++ Pertanian/resources/views/Layouts/footer.blade.php
@@ -1,55 +1,60 @@
   <!-- ======= Footer ======= -->
   <footer id="footer">
-    <div class="container">
-    <div class="f-items default-padding">
-            <div class="row">
-                <div class="col-lg-6 col-md-12 item">
-                    <div class="f-item about">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3986.4358168972344!2d99.13890127604!3d2.359450557497076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e01255a608ff5%3A0x1d795cd2efddf723!2sJl.%20Ujung%20Tanduk%2C%20Kec.%20Laguboti%2C%20Toba%2C%20Sumatera%20Utara%2022381!5e0!3m2!1sen!2sid!4v1683046710247!5m2!1sen!2sid" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 item">
-                    <div class="f-item">
-                        <h4 class="widget-title">Info Kontak </h4>
-                        <div class="address">
-                            <ul>
-                                <li>
-                                    <strong>Alamat: </strong>
-                                    Dusun III Sipituhuta,Kecamatan Pollung,Kabupaten Humbang Hasundutan Sumatera Utara
-                                </li>
-                                <li>
-                                    <strong>Email: </strong>
-                                    <a href="mailto:kmadumatani@gmail.com">SiTani@gmail.com</a>
-                                </li>
-                                <li>
-                                    <strong>Telepon: </strong>
-                                    <a href="https://wa.me/6285261370755"> +62 852-6137-0755</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="social">
-                            <ul>
-                                <li class="instagram">
-                                    <a href="https://instagram.com/madumatanii">
-                                        <i class="fab fa-instagram"></i>
-                                    </a>
-                                </li>
-                                <li class="facebook">
-                                    <a href="https://www.facebook.com/profile.php?id=100081576753478">
-                                        <i class="fab fa-facebook-f"></i>
-                                    </a>
-                                </li>
-                                <li class="youtube">
-                                    <a href="https://youtube.com/channel/UCE8S9qfI5X2WFkcqM5L1uZA">
-                                        <i class="fab fa-youtube"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </div>
-  </footer><!-- End Footer -->
+      <div class="container">
+          <div class="f-items default-padding">
+              <div class="row">
+                  <div class="col-lg-6 col-md-12 item">
+                      <div class="f-item about">
+                          <iframe
+                              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3986.4358168972344!2d99.13890127604!3d2.359450557497076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e01255a608ff5%3A0x1d795cd2efddf723!2sJl.%20Ujung%20Tanduk%2C%20Kec.%20Laguboti%2C%20Toba%2C%20Sumatera%20Utara%2022381!5e0!3m2!1sen!2sid!4v1683046710247!5m2!1sen!2sid"
+                              width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy"
+                              referrerpolicy="no-referrer-when-downgrade"></iframe>
+                      </div>
+                  </div>
+                  <div class="col-lg-3 col-md-6 item">
+                      <div class="f-item">
+                          <h4 class="widget-title">Info Kontak </h4>
+                          <div class="address">
+                              <ul>
+                                  <li>
+                                      <strong>Alamat: </strong>
+                                      Dusun III Sipituhuta,Kecamatan Pollung,Kabupaten Humbang Hasundutan Sumatera Utara
+                                  </li>
+                                  <li>
+                                      <strong>Email: </strong>
+                                      <a href="mailto:kmadumatani@gmail.com">SiTani@gmail.com</a>
+                                  </li>
+                                  <li>
+                                      <strong>Telepon: </strong>
+                                      <a href="https://wa.me/6285261370755"> +62 852-6137-0755</a>
+                                  </li>
+                              </ul>
+                          </div>
+                          <div class="social">
+                              <ul>
+                                  <li class="instagram">
+                                      <a href="https://instagram.com/madumatanii">
+                                          <i class="fab fa-instagram"></i>
+                                      </a>
+                                  </li>
+                                  <li class="facebook">
+                                      <a href="https://www.facebook.com/profile.php?id=100081576753478">
+                                          <i class="fab fa-facebook-f"></i>
+                                      </a>
+                                  </li>
+                                  <li class="youtube">
+                                      <a href="https://youtube.com/channel/UCE8S9qfI5X2WFkcqM5L1uZA">
+                                          <i class="fab fa-youtube"></i>
+                                      </a>
+                                  </li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+  </footer>
+  <!-- End Footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+          class="bi bi-arrow-up-short"></i></a>
